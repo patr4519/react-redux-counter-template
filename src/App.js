b7dd768d55d12e7f114ed73474d88499cf79c601
patr4519 <jero96@mail.ru>
2023-03-03 22:11:33 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
+      
     </div>
   );
 }

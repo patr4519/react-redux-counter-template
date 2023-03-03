e7e59a8f48bb0e7177c71df4e2ad9af9464ac78e
patr4519 <jero96@mail.ru>
2023-03-03 22:53:08 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='mainTitle'>Todo</h1>
-      <input className='mainInput' value={'input'}/>
-      <section className='main'>
-        <ul className='todo-list'>
-          <li>
-            <div className='view'>
-              <input className='toggle' type='checkbox'/>
-              <label>Todo 1</label>
-            </div>
-          </li>
-          <li>
-            <div className='view'>
-              <input className='toggle' type='checkbox'/>
-              <label>Todo 2</label>
-            </div>
-          </li>
-          <li>
-            <div className='view'>
-              <input className='toggle' type='checkbox'/>
-              <label>Todo 3</label>
-            </div>
-          </li>
-        </ul>
-        <footer className='footer'>
-          <span className='todo-left'>X items left</span>
-          <ul className='filters'>
-            <li>All</li>
-            <li>Active</li>
-            <li>Completed</li>
-          </ul>
-        </footer>
-      </section>
+      <Header />
+      <MainSection />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1 className="mainTitle">Todo</h1>;
+};
+
+export default Header;

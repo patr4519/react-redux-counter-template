2b095620255fae61e69c47de0111e945d18febda
patr4519 <jero96@mail.ru>
2023-03-03 22:25:50 +0300

    +

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='mainTitle'>Todo</h1>
+      <input className='mainInput' value={'input'}/>
+      <section className='main'>
+        <ul className='todo-list'>
+          <li>
+            <div className='view'>
+              <input className='toggle' type='checkbox'/>
+              <label>Todo 1</label>
+            </div>
+          </li>
+          <li>
+            <div className='view'>
+              <input className='toggle' type='checkbox'/>
+              <label>Todo 2</label>
+            </div>
+          </li>
+          <li>
+            <div className='view'>
+              <input className='toggle' type='checkbox'/>
+              <label>Todo 3</label>
+            </div>
+          </li>
+        </ul>
+        <footer className='footer'>
+          footer
+        </footer>
+      </section>
     </div>
   );
 }
